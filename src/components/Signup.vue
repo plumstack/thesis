1e86afb00e25a161daea0d0a8ab87b100c6a565f
Robert Chung <chungrob@gmail.com>
2018-06-21 23:38:02 -0500

    Added vue  pages for Login & Signup

---
+++ src/components/Signup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="signup">
+    <h1>Sign Up with Spotify</h1>
+    <div>wip wip wip</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: 'Montserrat', sans-serif;
+  color: #84bd00;
+  text-align: center;
+}
+</style>
