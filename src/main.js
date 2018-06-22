c7a3966de46729f9e42d44965ab7779b32b36ee6
Michael Panetta <mtpanetta@gmail.com>
2018-06-22 08:51:18 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue';
 
 import HelloWorld from '../src/components/HelloWorld.vue';
 import Room from '../src/components/Room.vue';
+import Login from '../src/components/Login.vue';
+import Signup from '../src/components/Signup.vue';
 
 Vue.config.productionTip = false;
 
