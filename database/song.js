872e1aa74448d88ab84d3317acb2f8abc302b6cc
Michael Panetta <mtpanetta@gmail.com>
2018-07-03 14:04:45 -0500

    added db song functions

--- database/song.js
+++ database/song.js
@@ -10,18 +10,38 @@ mongoose
 
 const SongSchema = new mongoose.Schema({
   roomid: String,
-  members: Array,
+  songid: String,
+  songtitle: String,
+  songartist: String,
+  songalbum: String,
+  albumurl: String,
+  nomsocket: String,
+  nomuser: String,
 });
 
 const Song = mongoose.model('Song', SongSchema);
 
-Song.create = function create(roomId, userName) {
-  const newRoom = new Song({
-    roomid: roomId,
-    members: [userName],
+Song.add = function create(roomID, songInfo) {
+  const newSong = new Song({
+    roomid: roomID,
+    songid: songInfo.id,
+    songtitle: songInfo.name,
+    songartist: songInfo.artists[0].name,
+    songalbum: songInfo.album.name,
+    albumurl: songInfo.album.images[2].url,
+    nomsocket: songInfo.clientInfo.id,
+    nomuser: songInfo.clientInfo.user,
   });
 
-  newRoom.save();
+  newSong.save();
+};
+
+Song.fetch = async function fetch(roomID, songID) {
+  const res = await Song.findOneAndRemove({
+    roomid: roomID,
+    songid: songID,
+  });
+  return res;
 };
 
 module.exports = Song;
