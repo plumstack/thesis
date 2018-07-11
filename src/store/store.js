3a1d417a04e940b55413c48ceafbb7652adffc3d
Jacob Johnston <johnstonajacob@gmail.com>
2018-07-10 21:26:54 -0500

    vuex queue store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+
 import userInfo from './modules/userInfo';
+import queueVotes from './modules/queueVotes';
 
 Vue.use(Vuex);
 
 
 export default new Vuex.Store({
-  modules: { userInfo },
+  modules: { userInfo, queueVotes },
 });
 
